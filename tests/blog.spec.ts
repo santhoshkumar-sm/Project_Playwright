2965dc50608f253ec3627fc6540564fa9e9137bc
Santhosh Kumar Sundaramurthy <santhoshkumar.sm@outlook.com>
2024-10-06 17:33:06 +0100

    Formatted all files using Prettier

--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -1,27 +1,27 @@
-import { test, expect } from '@playwright/test';
-import BlogPage from '../pages/blog.page';
-import HomePage from '../pages/home.page';
+import { test, expect } from "@playwright/test";
+import BlogPage from "../pages/blog.page";
+import HomePage from "../pages/home.page";
 
-test.describe('Tests for Blog', () => {
-    let blogPage: BlogPage;
-    let homePage: HomePage;
-    test('Go to Blog tab and get the total number of blogs', async ({ page }) => {
-        blogPage = new BlogPage(page);
-        homePage = new HomePage(page);
-        let blogText;
-        await homePage.navigateToHomePage();
-        await blogPage.blogHyperLink.click();
-        await expect(page).toHaveURL(/.*blog/);
+test.describe("Tests for Blog", () => {
+  let blogPage: BlogPage;
+  let homePage: HomePage;
+  test("Go to Blog tab and get the total number of blogs", async ({ page }) => {
+    blogPage = new BlogPage(page);
+    homePage = new HomePage(page);
+    let blogText;
+    await homePage.navigateToHomePage();
+    await blogPage.blogHyperLink.click();
+    await expect(page).toHaveURL(/.*blog/);
 
-        await blogPage.blogs.waitFor({state: "visible", timeout: 3000});
-        const blogList = await blogPage.blogList;
-        
-        console.log("Total number of blogs => "+await blogList.count());
-        expect(await blogList.count()).toEqual(5);
-        
-        for(const blog of await blogList.elementHandles()){
-             blogText = (await blog.textContent())?.trim();
-            expect(blogText.length).toBeGreaterThan(10);
-        }
-    })
-})
+    await blogPage.blogs.waitFor({ state: "visible", timeout: 3000 });
+    const blogList = await blogPage.blogList;
+
+    console.log("Total number of blogs => " + (await blogList.count()));
+    expect(await blogList.count()).toEqual(5);
+
+    for (const blog of await blogList.elementHandles()) {
+      blogText = (await blog.textContent())?.trim();
+      expect(blogText.length).toBeGreaterThan(10);
+    }
+  });
+});
--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,27 +1,27 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../pages/home.page";
 import CartPage from "../pages/cart.page";
-import path from 'path';
+import path from "path";
 
-test.describe('Tests for validating cart and upload functionality', () => {
-    let homePage: HomePage;
-    let cartPage : CartPage;
+test.describe("Tests for validating cart and upload functionality", () => {
+  let homePage: HomePage;
+  let cartPage: CartPage;
 
-    test('Validation of simple upload', async ({ page }) => {
-        homePage = new HomePage(page);
-        cartPage = new CartPage(page);
+  test("Validation of simple upload", async ({ page }) => {
+    homePage = new HomePage(page);
+    cartPage = new CartPage(page);
 
-        await homePage.navigateToHomePage();
-        await cartPage.cartHyperLink.click();
-        await page.waitForTimeout(2000);
+    await homePage.navigateToHomePage();
+    await cartPage.cartHyperLink.click();
+    await page.waitForTimeout(2000);
 
-        const pageHeader = (await cartPage.cartPageHeader.textContent())?.trim();
-        expect(pageHeader).toEqual("Cart");
-        cartPage.uploadBTN.scrollIntoViewIfNeeded();
-        const filePath = path.join(__dirname,"../testdata/sampleImage.jpg");
-        await page.setInputFiles("//*[@id='upfile_1']",filePath);
-        await cartPage.uploadBTN.click();
-        await cartPage.successMessage.isVisible();
-        await expect(cartPage.successMessage).toBeVisible();
-    })
-})
+    const pageHeader = (await cartPage.cartPageHeader.textContent())?.trim();
+    expect(pageHeader).toEqual("Cart");
+    cartPage.uploadBTN.scrollIntoViewIfNeeded();
+    const filePath = path.join(__dirname, "../testdata/sampleImage.jpg");
+    await page.setInputFiles("//*[@id='upfile_1']", filePath);
+    await cartPage.uploadBTN.click();
+    await cartPage.successMessage.isVisible();
+    await expect(cartPage.successMessage).toBeVisible();
+  });
+});
